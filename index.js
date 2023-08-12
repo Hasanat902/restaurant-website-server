f4c5a4d8738efbcb78c9ee1505f82d073e99c227
Hasanat902 <abulhasanat902@gmail.com>
2023-08-12 16:22:24 +0600

    implement jwt token

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -35,8 +52,25 @@ async function run() {
     const cartCollection = client.db("restaurantDb").collection("carts");
 
 
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
+    });
+
+    const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        if(user?.role !== 'admin'){
+          return res.status(403).send({error: true, message: 'forbidden access'})
+        }
+        next();
+    };
+
+
     // users api
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async(req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
     });
@@ -51,6 +85,18 @@ async function run() {
         }
         const result = await usersCollection.insertOne(user);
         res.send(result);
+    });
+
+    app.get('/users/admin/:email', verifyJWT, async(req, res) => {
+        const email = req.params.email;
+
+        if(req.decoded.email !== email){
+          res.send({ admin: false });
+        }
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === 'admin' };
+        res.send(result); 
     });
 
     app.patch('/users/admin/:id', async(req, res) => {
@@ -81,10 +127,15 @@ async function run() {
     });
 
     //Cart Collection
-    app.get('/carts', async(req, res) => {
+    app.get('/carts', verifyJWT, async(req, res) => {
         const email = req.query.email;
         if(!email){
           res.send([]);
+        }
+
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+          return res.status(403).send({ error: true, message: 'forbidden access' });
         }
         const query = { email: email };
         const result = await cartCollection.find(query).toArray();
