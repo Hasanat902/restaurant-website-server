b7a9adb5c8d2e30942d81593712854a0dafa3364
Hasanat902 <abulhasanat902@gmail.com>
2023-08-12 13:18:34 +0600

    create make admin api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -35,9 +36,13 @@ async function run() {
 
 
     // users api
+    app.get('/users', async(req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    });
+
     app.post('/users', async(req, res) => {
         const user = req.body;
-        console.log(user);
         const query = {email: user.email};
         const existingUser = await usersCollection.findOne(query);
         console.log('existing user', existingUser);
@@ -46,7 +51,20 @@ async function run() {
         }
         const result = await usersCollection.insertOne(user);
         res.send(result);
-    })
+    });
+
+    app.patch('/users/admin/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    });
 
 
     //menu api
