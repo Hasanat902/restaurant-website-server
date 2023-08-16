c82cbcacd9cd6134baf4ac02a7b90c43f9131a30
Hasanat902 <abulhasanat902@gmail.com>
2023-08-16 20:18:22 +0600

    create admin stats api

--- index.js
+++ index.js
@@ -192,7 +192,16 @@ async function run() {
         const deleteResult = await cartCollection.deleteMany(query); 
 
         res.send({insertResult, deleteResult});
-    })
+    });
+
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async(req, res) => {
+        const users = await usersCollection.estimatedDocumentCount();
+        const products = await menuCollection.estimatedDocumentCount();
+        const orders = await paymentCollection.estimatedDocumentCount();
+        const payments = await paymentCollection.find().toArray();
+        const revenue = payments.reduce( (sum, payment) => sum + payment.price, 0);
+        res.send({ revenue,users, products, orders });
+    });
 
 
     // Send a ping to confirm a successful connection
